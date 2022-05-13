0553b25bd5caced4f23c79fbffe73005347c4236
elina jaiani <elinajaiani@gmail.com>
2022-05-13 22:55:17 +0400

    js

---
+++ lesson8.js
@@ -0,0 +1,17 @@
+
+var one;
+one=6;
+document.getElementById("demom").innerHTML=one;
+
+var a;
+a=15;
+
+var x,y;
+x=("stream4");
+y=22;
+c=x+y;
+alert("c")
+
+document.getElementById('demom').innerHTML=c;
+
+
